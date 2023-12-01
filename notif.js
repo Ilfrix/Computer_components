03a044f008a6b7d68e142644085e36dd95826dbe
Vyacheslav <vya.polyanskiy@gmail.com>
2023-12-01 16:15:59 +0300

    (develop) Task 13, num 3, 4

--- notif.js
+++ notif.js
@@ -4,8 +4,23 @@ var start_value = 0;
 var count = 0;
 
 function create_notif(){
-  new_notif = document.createElement('a');
-  new_notif.innerText = `Уведомление ${++start_value}`;
+  new_notif = document.createElement('div');
+  new_notif.id = `div ${start_value}`;
+  new_notif.style.height = 40 + 'px';
+  new_notif.style.width = 170 + 'px';
+  mes = document.createElement('a');
+  but = document.createElement('button');
+  
+  but.style.position = 'absolute';
+  but.style.height = 20 + 'px';
+  but.style.width = 20 + 'px';
+  but.style.left = (new_notif.style.width.slice(0, -2) - but.style.width.slice(0, -2)) + 'px';
+  but.id = start_value;
+
+  but.textContent = 'X';
+  mes.innerText = `Уведомление ${++start_value}`;
+  new_notif.append(but);
+  new_notif.append(mes);
   elem.append(new_notif);
   text.innerHTML = `<strong>${start_value}</strong>`;
 }
@@ -38,3 +53,13 @@ function showNotification(){
   setTimeout (() => notification.remove(), 5500);
 }
 showNotification();
+
+var bell = document.getElementsByClassName('dropdown-content')[0];
+bell.addEventListener('click', function(event){
+  if (event.target != undefined && event.target.localName == 'button'){
+    let delet = document.getElementById(`div ${event.target.id}`);
+    delet.remove();
+    --start_value;
+    text.innerHTML = `<strong>${start_value}</strong>`
+  }
+});
