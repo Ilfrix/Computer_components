58b4f116739d1288eb78c5b6189e0a9bc848f038
Vyacheslav <vya.polyanskiy@gmail.com>
2023-12-11 20:01:34 +0300

    (develop) fix card touch

--- course/card.js
+++ course/card.js
@@ -4,17 +4,32 @@ var main = document.getElementById('main');
 var tmp = document.getElementById('title');
 
 main.onclick = function(event){
-  // let section = event.target;
-  let id_name = '#'+event.target.id;
-  let num = id_name.slice(-1);
-  console.log(id_name);
-  let card = main.querySelector(id_name);
-  card.style.left = 0;
-  card.style.transform = "translateX(0%)";
-  let text_name = "#text" + num;
-  console.log("text name" + text_name);
-  let title = main.querySelector(text_name);
-  title.style.display = 'block';
-  let text = main.querySelector(text_name).nextElementSibling;
-  text.style.display = 'block';
+  if (event.target.id.slice(0, -1) == 'card_vid'){
+    let id_name = '#'+event.target.id;
+    let num = id_name.slice(-1);
+    let card = main.querySelector(id_name);
+    card.style.left = 0;
+    card.style.transform = "translateX(0%)";
+    let text_name = "#text" + num;
+    let title = main.querySelector(text_name);
+    title.style.display = 'block';
+    let text = main.querySelector(text_name).nextElementSibling;
+    text.style.display = 'block';
+  }
+  
 };
+
+main.ondblclick = function(event){
+  if (event.target.id.slice(0, -1) == 'card_vid') {
+    let id_name = '#'+event.target.id;
+    let num = id_name.slice(-1);
+    let card = main.querySelector(id_name);
+    let text_name = "#text" + num;
+    let title = main.querySelector(text_name);
+    title.style.display = 'none';
+    let text = main.querySelector(text_name).nextElementSibling;
+    text.style.display = 'none';
+    card.style.left = "50%";
+    card.style.transform = "translateX(-50%)";
+  }
+}
